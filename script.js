ad128c4d5fc1187e16205cd4e87569bf18ee24cc
ponjae <>
2021-05-20 16:32:01 +0200

    Added som error-handling for the form

--- script.js
+++ script.js
@@ -12,5 +12,33 @@ function showModal() {
 }
 
 modalShow.addEventListener('click', showModal);
-modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+    bookmarkForm.reset();
+});
 window.addEventListener('click', (e) => (e.target) === modal ? modal.classList.remove('show-modal') : false);
+
+function formValidator(url) {
+    const expr = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expr);
+    if (url.match(regex)) {
+        return true;
+    } else {
+        alert('Please provide a valid website address');
+        return false;
+    }
+}
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameElem.value;
+    let urlValue = websiteUrlElem.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    const valid = formValidator(urlValue);
+
+}
+
+bookmarkForm.addEventListener('submit', storeBookmark);
+
